49a0924a25b16e0f22490e157c5e09bd2ff19fa0
chen <czw350nwpu>
2020-09-01 19:48:30 +0800

    注释说明

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/chen/catkin_ws/src
+# Build directory: /home/chen/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
+subdirs("test_pkg")
+subdirs("learning_service")
+subdirs("learning_topic")
---
+++ build/learing_service/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chen/catkin_ws/src/learing_service
+# Build directory: /home/chen/catkin_ws/build/learing_service
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
